d039afeaf068534299a57152c8ebf0870b657127
Anna Timofeeva <anjatimofeyeva@gmail.com>
2017-08-14 10:59:37 -0700

    refactoring for declaring class, Constructor and markDone()

--- app/to-do.ts
+++ app/to-do.ts
@@ -1,18 +1,21 @@
-//declaring Class
 class Task {
-  done: boolean;
-  description: string;
-  priority: string;
+  done: boolean = false;
 
-//declaring Constructor
-constructor(public descriptionParameter: string, public priorityParameter: string) {
-this.done = false;
-//add public keyword and remove this. from the Constructor
-// this.description = descriptionParameter;
-// this.priority = priorityParameter;
+  constructor(public description: string, public priority: string){}
+
+  markDone(){
+    this.done = true;
   }
+
 }
 
 var tasks: Task[] = [];
-tasks.push(new Task('Do the dishes.', 'medium'));
-console.log(tasks);
+tasks.push(new Task('Do the dishes.', 'Medium'));
+tasks.push(new Task('Buy chocolate.', 'Low'));
+tasks.push(new Task('Do laundry.', 'High'));
+
+tasks[0].markDone();
+
+for(var task of tasks){
+  console.log(task);
+}
